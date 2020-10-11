bd67f82c5e53d32a3c96ef159e11268dda026244
wwwwwmw <william.manwai@gmail.com>
2020-10-12 01:53:51 +0800

    init for dicrod clone

---
+++ src/firestore.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCYZRuOxwhmiEKSdHfxMsUWL-4TDgrzZr4",
+  authDomain: "dicsord-clone.firebaseapp.com",
+  databaseURL: "https://dicsord-clone.firebaseio.com",
+  projectId: "dicsord-clone",
+  storageBucket: "dicsord-clone.appspot.com",
+  messagingSenderId: "907415497435",
+  appId: "1:907415497435:web:5ebf85361b136fc59d3afe",
+  measurementId: "G-M1P28FTZXM"
+};
